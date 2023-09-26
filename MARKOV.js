93c63beff78e193085451ccc44c2dac43f74a35a
Christian Wise <djnarcissist@Christians-MBP.lan>
2023-09-26 15:26:09 -0400

    Part 0 & 1

--- MARKOV.js
+++ MARKOV.js
@@ -17,14 +17,53 @@ class MarkovMachine {
      *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
   
     makeChains() {
-      // TODO
-    }
+      this.chains = {};
+      for (let i = 0; i < this.words.length - 1; i++) {
+        const currentWord = this.words[i];
+        const nextWord = this.words[i + 1];
   
+        if (!this.chains[currentWord]) {
+          this.chains[currentWord] = [];
+        }
+  
+        this.chains[currentWord].push(nextWord);
+      }
+  
+      // Handle the last word (it has no next word)
+      const lastWord = this.words[this.words.length - 1];
+      this.chains[lastWord] = [null];
+    }
   
     /** return random text from chains */
   
     makeText(numWords = 100) {
-      // TODO
+      const text = [];
+      let currentWord = this.getRandomWord();
+  
+      while (text.length < numWords && currentWord !== null) {
+        text.push(currentWord);
+        currentWord = this.getNextWord(currentWord);
+      }
+  
+      return text.join(' ');
+    }
+  
+    getRandomWord() {
+      const words = Object.keys(this.chains);
+      const randomIndex = Math.floor(Math.random() * words.length);
+      return words[randomIndex];
+    }
+  
+    getNextWord(word) {
+      const possibleNextWords = this.chains[word];
+      const randomIndex = Math.floor(Math.random() * possibleNextWords.length);
+      return possibleNextWords[randomIndex];
     }
   }
+  
+  // Example usage:
+  const text = "the cat in the hat";
+  const markovMachine = new MarkovMachine(text);
+  const randomText = markovMachine.makeText(10); // Generate random text with 10 words
+  console.log(randomText);
   
--- make_Text.js
+++ make_Text.js
@@ -1,2 +1,8 @@
 /** Command-line tool to generate Markov text. */
+const fs = require("fs");
+const markov = require("./MARKOV");
+const axios = ("axios");
+const process = ("process");
+
+
 
